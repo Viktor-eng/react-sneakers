760d49fd135a0f02ed533cc290886305e8beb4ca
belvl <belvl@tomskcable.ru>
2023-02-13 17:36:49 +0700

    Промежуточная верстка

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 function App() {
     return (
         <div className="wrapper clear">
+            <div className="overlay">
+                <div className="drawer">
+                    <h2 className="mb-30">Корзина</h2>
+
+                    <div className="items">
+                        <div className="cartItem d-flex align-center mb-20">
+                            <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+                                 className="cartItemImg">
+                            </div>
+                            <div className="mr-20 flex">
+                                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                                <b>12 999 руб.</b>
+                            </div>
+                            <div>
+                                <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
+                            </div>
+                        </div>
+
+                        <div className="cartItem d-flex align-center">
+                            <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+                                 className="cartItemImg">
+                            </div>
+                            <div className="mr-20 flex">
+                                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                                <b>12 999 руб.</b>
+                            </div>
+                            <div>
+                                <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <ul className="cartTotalBlock">
+                        <li>
+                            <span>Итого</span>
+                            <div></div>
+                            <b>21 488 руб.</b>
+                        </li>
+                        <li>
+                            <span>Налог 5%:</span>
+                            <div></div>
+                            <b>1074 руб.</b>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             <header className="d-flex justify-between align-center p-40">
                 <div className="d-flex align-center">
-                    <img width={40} height={40} src="/img/logo.png"/>
+                    <img width={40} height={40} src="/img/logo.png" alt=""/>
                     <div>
                         <h3 className="text-uppercase">React Sneakers</h3>
                         <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -11,19 +57,29 @@ function App() {
                 </div>
                 <ul className="d-flex">
                     <li className="mr-30">
-                        <img width={18} height={18} src="/img/cart.svg"/>
+                        <img width={18} height={18} src="/img/cart.svg" alt=""/>
                         <span>1205 руб.</span>
+                        <img src="/img/btn-remove.svg" alt="Remove"/>
                     </li>
                     <li>
-                        <img width={18} height={18} src="/img/user.svg"/>
+                        <img width={18} height={18} src="/img/user.svg" alt=""/>
                     </li>
                 </ul>
             </header>
             <div className="content p-40">
-                <h1 className="mb-40">Все кроссовки</h1>
+                <div className= "d-flex align-center justify-between mb-40">
+                    <h1>Все кроссовки</h1>
+                    <div className="search-block d-flex">
+                        <img src="/img/search.svg" alt="Search"/>
+                        <input placeholder ="Поиск..." />
+                    </div>
+                </div>
 
                 <div className="d-flex">
                     <div className="card">
+                        <div className="favorite">
+                        <img src="/img/heart-unliked.svg" alt="Unliked"/>
+                        </div>
                         <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
                         <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
                         <div className="d-flex justify-between align-center">
