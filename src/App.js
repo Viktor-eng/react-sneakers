9fb05cec2f9093309f8a0462025557f408add77a
belvl <belvl@tomskcable.ru>
2023-02-17 09:29:48 +0700

    Добавлена папка для компонентов, Card переименован в index. SCSS Card переделан в модульный SCSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import Card from "./components/Card"
+import Index from "./components/Card"
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
+
+const arr = [
+    {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg'},
+    {title: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg'},
+    {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg'},
+    {title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg'},
+]
 
 function App() {
     return (
         <div className="wrapper clear">
-
             <Drawer/>
             <Header/>
-
             <div className="content p-40">
                 <div className="d-flex align-center justify-between mb-40">
                     <h1>Все кроссовки</h1>
@@ -17,12 +22,13 @@ function App() {
                         <input placeholder="Поиск..."/>
                     </div>
                 </div>
-
                 <div className="d-flex">
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
+
+                    {arr.map(obj =>
+                        <Index title={obj.title}
+                               price={obj.price}
+                               imageUrl={obj.imageUrl}
+                               onClick={() => console.log(obj)}/>)}
                 </div>
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-function Header(){
-    return(
+function Header() {
+    return (
         <header className="d-flex justify-between align-center p-40">
             <div className="d-flex align-center">
                 <img width={40} height={40} src="/img/logo.png" alt=""/>
