f3ff6f59de548adce06c57969c82444ca395f997
belvl <belvl@tomskcable.ru>
2023-02-19 17:35:59 +0700

    Настроена кнопка отображения корзины

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import Index from "./components/Card"
+import React from 'react';
+import Card from "./components/Card"
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
+
 
 const arr = [
     {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg'},
@@ -10,10 +12,11 @@ const arr = [
 ]
 
 function App() {
+    const [cartOpened, setCartOpened] = React.useState(false);
     return (
         <div className="wrapper clear">
-            <Drawer/>
-            <Header/>
+            {cartOpened ? <Drawer/> : null}
+            <Header onClickCart={() => setCartOpened(true)}/>
             <div className="content p-40">
                 <div className="d-flex align-center justify-between mb-40">
                     <h1>Все кроссовки</h1>
@@ -25,10 +28,11 @@ function App() {
                 <div className="d-flex">
 
                     {arr.map(obj =>
-                        <Index title={obj.title}
-                               price={obj.price}
-                               imageUrl={obj.imageUrl}
-                               onClick={() => console.log(obj)}/>)}
+                        <Card title={obj.title}
+                              price={obj.price}
+                              imageUrl={obj.imageUrl}
+                              onFavorite={() => console.log('Добавили в закладки')}
+                              onPlus={() => console.log('Нажали плюс')}/>)}
                 </div>
             </div>
         </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,16 @@
 import styles from './Card.module.scss'
+import React from "react";
 
-function Index(props) {
-    console.log(props);
+function Card(props) {
+    const [isAdded, setIsAdded] = React.useState(false);
+
+    const onClickPlus = () => {
+        setIsAdded(!isAdded);
+    }
 
     return (
         <div className={styles.card}>
-            <div className={styles.favorite}>
+            <div className={styles.favorite} onClick={props.onFavorite}>
                 <img src="/img/heart-unliked.svg" alt="Unliked"/>
             </div>
             <img width={133} height={112} src={props.imageUrl} alt="Sneakers"/>
@@ -15,12 +20,11 @@ function Index(props) {
                     <span>Цена:</span>
                     <b>{props.price} руб.</b>
                 </div>
-                <button className="button" onClick={props.onClick}>
-                    <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
-                </button>
+                <img className={styles.plus} onClick={onClickPlus}
+                     src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="Plus"/>
             </div>
         </div>
     );
 }
 
-export default Index;
+export default Card;
