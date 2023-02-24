6e989fbaa1e87ffa167317758880387402c91079
belvl <belvl@tomskcable.ru>
2023-02-24 16:27:28 +0700

    Добавление и удаление вещей в корзину реализовано через бэкэнд

--- src/App.js
+++ src/App.js
@@ -1,38 +1,70 @@
 import React from 'react';
+import axios from "axios";
 import Card from "./components/Card"
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
-
-const arr = [
-    {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg'},
-    {title: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg'},
-    {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg'},
-    {title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg'},
-]
-
 function App() {
+    const [items, setItems] = React.useState([])
+    const [cartItems, setCartItems] = React.useState([]);
+    const [favorites, setFavorites] = React.useState([]);
+    const [searchValue, setSearchValue] = React.useState('');
     const [cartOpened, setCartOpened] = React.useState(false);
+
+    React.useEffect(() => {
+        axios.get('https://63f2e494f28929a9df6156f9.mockapi.io/items').then((res) => {
+            setItems(res.data);
+        });
+        axios.get('https://63f2e494f28929a9df6156f9.mockapi.io/cart').then((res) => {
+            setCartItems(res.data);
+        });
+    },[]);
+
+    const onAddToCart = (obj) => {
+        axios.post('https://63f2e494f28929a9df6156f9.mockapi.io/cart', obj);
+        setCartItems((prev) => [...prev, obj]);
+    };
+
+    const onRemoveItem =  (id) => {
+        axios.delete(`https://63f2e494f28929a9df6156f9.mockapi.io/cart/${id}`);
+        setCartItems((prev) => prev.filter((item) => item.id !== id));
+    };
+
+    const onChangeSearchInput = (event) => {
+        setSearchValue(event.target.value);
+    }
+
     return (
         <div className="wrapper clear">
-            {cartOpened ? <Drawer/> : null}
+            {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem}/>}
             <Header onClickCart={() => setCartOpened(true)}/>
             <div className="content p-40">
                 <div className="d-flex align-center justify-between mb-40">
-                    <h1>Все кроссовки</h1>
+                    <h1>{searchValue ? `Поиск по запросу: "${searchValue}` : 'Все кроссовки'}</h1>
                     <div className="search-block d-flex">
                         <img src="/img/search.svg" alt="Search"/>
-                        <input placeholder="Поиск..."/>
+                        {searchValue &&
+                            <img
+                                onClick={() => setSearchValue("")}
+                                className="clear cu-p"
+                                src="/img/btn-remove.svg"
+                                alt="Clear"
+                            />}
+                        <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..."/>
                     </div>
                 </div>
-                <div className="d-flex">
+                <div className="d-flex flex-wrap">
 
-                    {arr.map(obj =>
-                        <Card title={obj.title}
-                              price={obj.price}
-                              imageUrl={obj.imageUrl}
-                              onFavorite={() => console.log('Добавили в закладки')}
-                              onPlus={() => console.log('Нажали плюс')}/>)}
+                    {items
+                        .filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+                        .map((item, index) => (
+                            <Card
+                                key={index}
+                                title={item.title}
+                                price={item.price}
+                                imageUrl={item.imageUrl}
+                                onFavorite={() => console.log('Добавили в закладки')}
+                                onPlus={(obj) => onAddToCart(obj)}/>))}
                 </div>
             </div>
         </div>
